da5dd62dac59e8ac3a47a00422dbafd903ff7fe0
Ryan <tamielryan@gmail.com>
2022-02-06 21:59:51 +0200

    TableView Programmatically and Constraints

--- NewsUIKit/ViewController.swift
+++ NewsUIKit/ViewController.swift
@@ -7,13 +7,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class NewsListVC: UIViewController {
+    let tableView = UITableView()
+    var safeArea: UILayoutGuide!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+        safeArea = view.layoutMarginsGuide
+        
+        tableView.dataSource = self
+        
+        setupView()
     }
-
-
+    
+    // MARK: - Setup View
+    func setupView() {
+        // Always add the UIView First before setting constraints
+        view.addSubview(tableView)
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.topAnchor.constraint(equalTo: safeArea.topAnchor).isActive = true
+        tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+    }
 }
 
+// MARK: - UITableViewDataSource
+
+extension NewsListVC: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 7
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+}
